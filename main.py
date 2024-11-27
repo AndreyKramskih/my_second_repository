3aadbceebf5cff8d8828cf19fffc26931a9bbd05
Andrey Kramskih <andreykramskih@gmail.com>
2024-11-27 13:14:18 +0300

    логгеры вывод в консоль

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-print(dir())
+import logging
+import sys
 
-import functions as f
-from data import my_dict
-from classes import *
+# Определяем первый вид форматирования
+format_1 = '#%(levelname)-8s [%(asctime)s] - %(filename)s:'\
+           '%(lineno)d - %(name)s - %(message)s'
+# Определяем второй вид форматирования
+format_2 = '[{asctime}] #{levelname:8} {filename}:'\
+           '{lineno} - {name} - {message}'
 
-print('Это исполняемый файл')
+# Инициализируем первый форматтер
+format_1=logging.Formatter(fmt=format_1)
+# Инициализируем второй форматтер
+format_2=logging.Formatter(fmt=format_2, style='{')
 
-new_variable:int = 15
+# Создаем логгер
+logger=logging.getLogger(__name__)
+
+# Инициализируем хэндлер, который будет перенаправлять логи в stderr
+stderr_handler=logging.StreamHandler()
+# Инициализируем хэндлер, который будет перенаправлять логи в stdout
+stdout_handler=logging.StreamHandler(sys.stdout)
+
+# Устанавливаем форматтеры для хэндлеров
+stderr_handler.setFormatter(format_1)
+stdout_handler.setFormatter(format_1)
+
+# Добавляем хэндлеры логгеру
+logger.addHandler(stderr_handler)
+logger.addHandler(stdout_handler)
+
+# Создаем лог
+logger.warning('Это лог с предупреждением')
 
 
-if __name__ == '__main__':
-    print('Код ниже не выполнится, если этот файл будет импортируемым модулем в другой исполняемый файл')
-    print(f.get_double_number(100))
-    print(my_dict)
-    MyClass()
-    print(dir())
+
+
+
+
 
