615eb60f73eab75e55617bbb9c4d7caabbd6e58b
Andrey Kramskih <andreykramskih@gmail.com>
2024-12-02 13:12:52 +0300

    inline клавиатура на лету

--- main.py
+++ main.py
@@ -1,66 +1,91 @@
 
-from aiogram import Bot, Dispatcher, F
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
+from aiogram.methods import DeleteStickerSet
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 BOT_TOKEN='7656570135:AAHAU-5sKUFNc5UfgpBqEd4fxrKQWcQpoeU'
 
 # Создаем объекты бота и диспетчера
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-
-# Создаем объекты инлайн-кнопок
-big_button_1 = InlineKeyboardButton(
-    text='БОЛЬШАЯ КНОПКА 1',
-    callback_data='big_button_1_pressed'
+bot=Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
 )
+dp=Dispatcher()
 
-big_button_2 = InlineKeyboardButton(
-    text='БОЛЬШАЯ КНОПКА 2',
-    callback_data='big_button_2_pressed'
-)
+LEXICON: dict[str, str] = {
+    'but_1': 'Кнопка 1',
+    'but_2': 'Кнопка 2',
+    'but_3': 'Кнопка 3',
+    'but_4': 'Кнопка 4',
+    'but_5': 'Кнопка 5',
+    'but_6': 'Кнопка 6',
+    'but_7': 'Кнопка 7',}
 
-# Создаем объект инлайн-клавиатуры
-keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[[big_button_1],
-                     [big_button_2]]
-)
+BUTTONS: dict[str, str] = {
+    'btn_1': '1',
+    'btn_2': '2',
+    'btn_3': '3',
+    'btn_4': '4',
+    'btn_5': '5',
+    'btn_6': '6',
+    'btn_7': '7',
+    'btn_8': '8',
+    'btn_9': '9',
+    'btn_10': '10',
+    'btn_11': '11'}
+
+
+# Функция для генерации инлайн-клавиатур "на лету"
+def create_inline_kb(width: int,
+                     *args: str,
+                     **kwargs: str) -> InlineKeyboardMarkup:
+    # Инициализируем билдер
+    kb_builder = InlineKeyboardBuilder()
+    # Инициализируем список для кнопок
+    buttons: list[InlineKeyboardButton] = []
+
+    # Заполняем список кнопками из аргументов args и kwargs
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(
+                text=LEXICON[button] if button in LEXICON else button,
+                callback_data=button))
+    if kwargs:
+        for button, text in kwargs.items():
+            buttons.append(InlineKeyboardButton(
+                text=text,
+                callback_data=button))
+
+    # Распаковываем список с кнопками в билдер методом row c параметром width
+    kb_builder.row(*buttons, width=width)
+
+    # Возвращаем объект инлайн-клавиатуры
+    return kb_builder.as_markup()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
-# и отправлять в чат клавиатуру с инлайн-кнопками
+# и отправлять в чат клавиатуру
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
-    await message.answer(
-        text='Это инлайн-кнопки. Нажми на любую!',
+   #keyboard = create_inline_kb(2, 'but_1', 'but_3', 'but_7')
+   keyboard = create_inline_kb(
+       2,
+       btn_tel='Телефон',
+       btn_email='email',
+       btn_website='Web-сайт',
+       btn_vk='VK',
+       btn_tgbot='Наш телеграм-бот'
+   )
+   await message.answer(
+        text='Это инлайн-клавиатура, сформированная функцией '
+             '<code>create_inline_kb</code>',
         reply_markup=keyboard
-)
-
-# Этот хэндлер будет срабатывать на апдейт типа CallbackQuery
-# с data 'big_button_1_pressed'
-@dp.callback_query(F.data == 'big_button_1_pressed')
-async def process_button_1_press(callback: CallbackQuery):
-    if callback.message.text != 'Была нажата БОЛЬШАЯ КНОПКА 1':
-        await callback.message.edit_text(
-            text='Была нажата БОЛЬШАЯ КНОПКА 1',
-            reply_markup=callback.message.reply_markup
-        )
-    await callback.answer()
+    )
 
 
-# Этот хэндлер будет срабатывать на апдейт типа CallbackQuery
-# с data 'big_button_2_pressed'
-@dp.callback_query(F.data == 'big_button_2_pressed')
-async def process_button_2_press(callback: CallbackQuery):
-    if callback.message.text != 'Была нажата БОЛЬШАЯ КНОПКА 2':
-        await callback.message.edit_text(
-            text='Была нажата БОЛЬШАЯ КНОПКА 2',
-            reply_markup=callback.message.reply_markup
-        )
-    await callback.answer()
-
-
-if __name__ == '__main__':
-    dp.run_polling(bot)
-
+dp.run_polling(bot)
 
